f6a0fc781618111c3ef48b8dacad2507dee012b6
DESKTOP-P9RL5J2\HP <bumkyung7777@gmail.com>
2026-07-11 14:24:55 +0900

    페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import productService from '@/services/productService';
 import ProductCard from '@/components/productCard/productCard';
 import { Suspense } from 'react';
 import Loading from '@/components/loading/loading';
-
+import Link from 'next/link';
 export default async function Home({ searchParams }: { searchParams: { category?: string } }) {
     const products = await productService(searchParams.category);
 
@@ -10,7 +10,9 @@ export default async function Home({ searchParams }: { searchParams: { category?
         <Suspense fallback={<Loading />}>
             <div className="grid grid-cols-4 gap-20 p-5">
                 {products.map((product: any) => (
-                    <ProductCard product={product} />
+                    <Link href={`/products/${product.id}`}>
+                        <ProductCard product={product} />
+                    </Link>
                 ))}
             </div>
         </Suspense>
